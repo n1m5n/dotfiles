78a7cb97cdd1ca1db960ce49e3e88433065b6d97
Naman Rohit <namansrohit@gmail.com>
2025-11-04 17:49:59 -0500

    Added fastfetch and terminal configs.

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -30,3 +30,5 @@ if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
     fastfetch -c "C:/Users/Naman/.config/fastfetch/config.jsonc"
 }
 
+# Start oh-my-posh
+oh-my-posh init pwsh --config "C:\Users\Naman\.config\oh-my-posh\vague_bubbles.json" | Invoke-Expression
